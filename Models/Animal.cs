25b88894326cdb5c989d89a84b918f870dc27203
Alexis Lazcano <0370102@cegepmontpetit.ca>
2024-03-22 09:38:15 -0400

    FCT Model Animal

---
+++ Models/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace H24_4204W6ProgWebServices_Labo08.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
